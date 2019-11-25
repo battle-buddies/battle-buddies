0bd5d07aaf72cf2cd926df958c1cf5c1259befc3
danielle lane <daniellelane1821@gmail.com>
2019-11-25 15:47:42 -0600

    something weird

--- src/main/java/com/codeup/blog/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.codeup.blog.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
