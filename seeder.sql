694878a07d79ab7e6f07599a09c89976184f3579
Isaiah Brashears <izzyb@Isaiahs-MacBook-Pro.local>
2019-11-25 14:48:16 -0600

    add meetups basic functionality

--- seeder.sql
+++ seeder.sql
@@ -153,11 +153,11 @@ insert into profile_trait (trait_id, profile_id) values ( 20, 10);
 
 
 
-insert into locations (location) values ('Salt Lake City,Utah,United States');
-insert into locations (location) values ('San Antonio,Texas,United States');
-insert into locations (location) values ('Charlotte,North Carolina,United States');
-insert into locations (location) values ('Miami,Florida,United States');
-insert into locations (location) values ('Honolulu,Hawaii,United States');
+insert into locations (location) values ('Salt Lake City, Utah, United States');
+insert into locations (location) values ('San Antonio, Texas, United States');
+insert into locations (location) values ('Charlotte, North Carolina, United States');
+insert into locations (location) values ('Miami, Florida, United States');
+insert into locations (location) values ('Honolulu, Hawaii, United States');
 
 
 insert into location_profile (profile_id, location_id) values (1, 2);
