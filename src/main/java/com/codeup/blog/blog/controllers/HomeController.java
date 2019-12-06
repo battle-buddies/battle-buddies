459d80b32334a55addd44023338f9ce9fadec009
Isaiah Brashears <izzyb@Isaiahs-MacBook-Pro.local>
2019-12-05 19:36:28 -0600

    error page

--- src/main/java/com/codeup/blog/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class HomeController {
@@ -26,7 +28,7 @@ public class HomeController {
     }
 
     @GetMapping("/error")
-    public String error(Exception exception, Model vModel){
+    public String error(Exception exception, Model vModel, HttpServletRequest request){
         vModel.addAttribute("error", exception);
         return "error";
 
